d2557ef82c82f4e5dd643fecb1a2699f1846d4be
agustin garcia maggi <agustingmaggi@gmail.com>
2023-08-01 21:37:59 +0200

    subiendo cambios que si funcionan

--- js/carrito.js
+++ js/carrito.js
@@ -14,27 +14,28 @@ carrito.forEach(prod => {
     Precio Unitario: $ ${prod.precio} <br><br>
     Precio Total : $ ${precioTotalPorProducto} <br><br>
     <button id="eliminarDelCarrito-${prod.id}"> Eliminar del carrito </button>
-    <div id="mensaje"></div>
+    <div id = "mensaje-${prod.id}" ></div >
     `
 
     renderizarCarrito.append(prodsCart)
     prodsCart.classList.add('prodsCart')
+    const mensajeDiv = document.getElementById(`mensaje-${prod.id}`)
 
     let boton = document.querySelector(`#eliminarDelCarrito-${prod.id}`)
-    boton.addEventListener('click', () => eliminarDelCarrito(prod.nombre))
+    boton.addEventListener('click', () => eliminarDelCarrito(prod.nombre, mensajeDiv))
 })
 
 
-const eliminarDelCarrito = (nombreProd) => {
+const eliminarDelCarrito = (nombreProd, msj) => {
     const producto = carrito.find((produc) => produc.nombre === nombreProd)
     if (producto && producto.cantidad === 1) {
         carrito.splice(carrito.indexOf(producto), 1)
         localStorage.setItem('carrito', JSON.stringify(carrito))
-        mostrarMensaje("Eliminaste el producto en su totalidad.")
+        mostrarMensaje(msj, "Eliminaste el producto en su totalidad.")
     } else if (producto && producto.cantidad > 1) {
         producto.cantidad -= 1
         localStorage.setItem('carrito', JSON.stringify(carrito))
-        mostrarMensaje("Redujiste la cantidad en 1.")
+        mostrarMensaje(msj, "Redujiste la cantidad en 1.")
     }
 
 }
@@ -42,9 +43,8 @@ const total = document.createElement('p')
 total.innerHTML = ` Total Carrito : ${totalCarrito}`
 renderizarCarrito.append(total)
 
-const mostrarMensaje = (mensaje) => {
-    const mensajeDiv = document.getElementById('mensaje');
-    mensajeDiv.textContent = mensaje;
+const mostrarMensaje = (contenedorDelMensaje, mensaje) => {
+    contenedorDelMensaje.textContent = mensaje;
 };
 
 
